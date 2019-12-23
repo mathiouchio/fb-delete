5bc39d0415a4df8cbaaba773c6b053a1b7725a1e
mathiouchio <contact@matheusdesain.com>
2019-12-23 13:44:13 -0500

    update

--- index.js
+++ index.js
@@ -1,82 +1,232 @@
-'use strict';
+const puppeteer = require("puppeteer");
+const prompts = require("./prompts");
+const date = require("./date");
+const { DEBUG } = process.env;
 
-const puppeteer = require('puppeteer');
-const prompts = require('./prompts');
-var page;
+let navStore = {};
+let page;
+let answers;
+let browser;
+let results = {};
+const debug = DEBUG === "true";
 
-const { EMAIL, PASSWORD } = process.env
-
-async function main() {
-
-  let answers = await prompts();
-
-  const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 100
+const runReport = () => {
+  Object.keys(results).forEach(key => {
+    const count = results[key].length;
+    console.log(`Deleted ${key} ${count} number of times.`);
   });
-  page = await browser.newPage();
+};
 
-  await page.goto('https://mbasic.facebook.com/');
-  await page.$eval('input[id=m_login_email]', (el, user) => el.value = user, EMAIL || answers.username);
-  await page.$eval('input[name=pass]', ((el, pass) => el.value = pass), PASSWORD || answers.password);
-  await page.$eval('input[name=login]', button => button.click());
-  await page.goto('https://mbasic.facebook.com/');
+const escapeXpathString = str => {
+  const splitedQuotes = str.replace(/'/g, `', "'", '`);
+  return `concat('${splitedQuotes}', '')`;
+};
 
-  await next(answers.categories, answers.years);
-}
+const getAttributes = async (handlers, property) => {
+  const propertyJsHandles = await Promise.all(
+    handlers.map(handle => handle.getProperty(property))
+  );
+  const attributes = await Promise.all(
+    propertyJsHandles.map(handle => handle.jsonValue())
+  );
 
-async function next(categories, years) {
-  await followLinkByContent('Profile');
-  await followLinkByContent('Activity Log');
-  await followLinkByContent('Filter');
+  return attributes;
+};
 
-  for (let i in categories) {
-    console.log("Deleting category " + categories[i]);
-    await followLinkByContent(categories[i]);
-    for (let j in years) {
-      console.log("In year " + years[j]);
-      try {
-        await followLinkByContent(years[j]);
-        await deleteYear(years[j]);
-      } catch(e) {
-        console.log(`Year ${years[j]} not found.`, e);
-      }
-    }
-    await followLinkByContent(categories[i]);
+const followLinkByContent = async (
+  text,
+  opt = { waitUntil: null, store: null }
+) => {
+  if (opt.waitUntil === "load") {
+    if (debug) console.log("followLinkByContent wait for load", text);
+    await page.waitForNavigation({ waitUntil: "load" });
+  } else {
+    if (debug) console.log("followLinkByContent no wait for load", text);
   }
 
-  await page.close();
+  const escapedText = escapeXpathString(text);
+  const linkHandlers = await page.$x(`//a[contains(text(), ${escapedText})]`);
+
+  if (linkHandlers.length > 0) {
+    if (opt.store) {
+      const href = await getAttributes(linkHandlers, "href");
+      opt.store[text] = href[0];
+    }
+    await linkHandlers[0].click();
+  } else {
+    throw new Error(errorLog(`Link not found: ${text}`));
+  }
+};
+
+const storeCategoryLinks = async () => {
+  await page.waitForNavigation({ waitUntil: "load" });
+  const $parentWrapper = await page.$("#objects_container");
+  const categoryKeys = await $parentWrapper.$$eval("a", els =>
+    els.map(el => el.innerText)
+  );
+  const categoryLinks = await $parentWrapper.$$eval("a", els =>
+    els.map(el => el.href)
+  );
+  categoryKeys.forEach((key, index) => {
+    navStore[key] = categoryLinks[index];
+  });
+};
+
+const followCategoryLinkByContent = async text => {
+  if (debug) console.log("followCategoryLinkByContent", text);
+  await page.goto(navStore[text], { waitUntil: "load" });
+};
+
+// Giving pop to error log
+const errorLog = msg => ("\x1b[31m", msg, "\x1b[0m");
+
+const selectOption = async () => {
+  answers = await prompts();
+
+  let redo = false;
+  Object.values(answers).forEach(x => {
+    if (x.length === 0) {
+      redo = true;
+    }
+  });
+
+  if (redo) {
+    console.log(
+      errorLog("Nothing is selected. Press <space> to select options!")
+    );
+    console.log(answers);
+    await selectOption();
+  }
+};
+
+(async () => {
+  await selectOption();
+  const args = answers.visual
+    ? {
+        headless: false,
+        slowMo: 100
+      }
+    : undefined;
+
+  browser = await puppeteer.launch(args);
+  page = await browser.newPage();
+  page.setDefaultNavigationTimeout(5000);
+
+  await page.goto("https://mbasic.facebook.com/", { waitUntil: "load" });
+  await page.$eval(
+    "input[id=m_login_email]",
+    (el, user) => (el.value = user),
+    answers.username
+  );
+  await page.$eval(
+    "input[name=pass]",
+    (el, pass) => (el.value = pass),
+    answers.password
+  );
+  await page.$eval("input[name=login]", button => button.click());
+  await page.goto("https://mbasic.facebook.com/", { waitUntil: "load" });
+
+  await next(answers.categories, answers.years);
+})();
+
+async function next(categories, years) {
+  await followLinkByContent("Profile");
+  await followLinkByContent("Activity Log", { waitUntil: "load" });
+  await followLinkByContent("Filter", { waitUntil: "load", store: navStore });
+  await storeCategoryLinks();
+
+  for (let i in categories) {
+    const category = categories[i];
+    results[category] = [];
+
+    if (i > 0) {
+      page.goto(navStore.Filter, { waitUntil: "load" });
+    }
+    await followCategoryLinkByContent(category);
+
+    for (let j in years) {
+      const year = years[j];
+      try {
+        if (date.currentYear.toString() !== answers.years[j]) {
+          await followLinkByContent(year);
+        }
+        await deleteYear(year, category);
+      } catch (e) {
+        console.log(`Finished looking for ${category} in year ${year}.`);
+      }
+    }
+  }
+
+  await browser.close();
   console.log("Done!");
+  console.log("Deleted: ", results);
+  runReport();
   process.exit();
 }
 
-async function deletePosts() {
+async function deletePosts(category) {
   // get all "allactivity/delete" and "allactivity/removecontent" links on page
-  var deleteLinks = await page.evaluate(() => {
-    var links = [];
-    const deleteElements = document.querySelectorAll('a[href*="allactivity/delete"]');
-    const removeElements = document.querySelectorAll('a[href*="allactivity/removecontent"]');
+  const deleteLinks = await page.evaluate(() => {
+    const links = [];
+    const deleteElements = document.querySelectorAll(
+      'a[href*="allactivity/delete"]'
+    );
+    const removeElements = document.querySelectorAll(
+      'a[href*="allactivity/removecontent"]'
+    );
     for (const el of deleteElements) {
-        links.push(el.href);
+      links.push(el.href);
     }
     for (const el of removeElements) {
-        links.push(el.href);
+      links.push(el.href);
     }
+
     return links;
   });
-  // visit them all to delete content
-  for (let i = 0; i < deleteLinks.length; i++) {
-      await page.goto(deleteLinks[i]);
+
+  async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+
+  if (deleteLinks.length === 0) {
+    console.log(`No ${category} content.`);
+  } else {
+    if (debug) console.log("deleteLinks", deleteLinks);
+
+    await asyncForEach(deleteLinks, async (link, index) => {
+      results[category].push(link);
+      console.log(`Deleting post number ${index} with link: ${link}`);
+      // visit them all to delete content
+      try {
+        await page.goto(link, { waitUntil: "load" });
+      } catch (error) {
+        throw new Error(errorLog("Can't delete."));
+      }
+    });
   }
 }
 
-
 async function getMonthLinks(year) {
-  var monthLinks = await page.evaluate((year) => {
-    var months = ["January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"];
-    var links = [];
-    const elements = document.querySelectorAll('a');
+  const monthLinks = await page.evaluate(year => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+
+    const links = [];
+    const elements = document.querySelectorAll("a");
     for (let el of elements) {
       for (let i = 0; i < months.length; i++) {
         if (months[i] + " " + year === el.innerText) {
@@ -84,30 +234,22 @@ async function getMonthLinks(year) {
         }
       }
     }
+
     return links;
   }, year);
+
   return monthLinks;
 }
 
-async function followLinkByContent(content) {
-  var link = await page.evaluate((text) => {
-    const aTags = document.querySelectorAll('a');
-    for (let aTag of aTags) {
-      if (aTag.innerText === text) {
-        return aTag.href;
-      }
-    }
-  }, content);
-  await page.goto(link);
-}
+async function deleteYear(year, category) {
+  if (year !== date.currentYear.toString()) {
+    await page.waitForNavigation({ waitUntil: "load" });
+  }
+  const monLinks = await getMonthLinks(year);
 
-async function deleteYear(year) {
-  var monLinks = await getMonthLinks(year);
   for (let mon in monLinks) {
-    // console.log("Deleting month: ", monLinks[mon]);
-    await page.goto(monLinks[mon]);
-    await deletePosts();
+    if (debug) console.log(`Going to month ${mon} in year ${year}.`);
+    await page.goto(monLinks[mon], { waitUntil: "load" });
+    await deletePosts(category);
   }
 }
-
-main();
